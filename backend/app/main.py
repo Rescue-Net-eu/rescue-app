264d51630bbc97c72b2b4ba553d7fd1085423014
Bogdan Cazacu <contact@cazacubogdan.com>
2025-06-04 12:45:37 +0000

    refactoring v0.1

--- backend/app/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI
-from app.routes import users, missions
-
-app = FastAPI()
-
-app.include_router(users.router, prefix="/users", tags=["users"])
-app.include_router(missions.router, prefix="/missions", tags=["missions"])
